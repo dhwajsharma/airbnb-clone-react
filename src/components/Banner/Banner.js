b167b901579c6def3390de29b8d7eb081563c24d
DhwajSharma <dhwajsharma10@gmail.com>
2021-07-20 19:54:48 +0530

    Building Search component

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,10 +1,20 @@
 import { Button } from '@material-ui/core'
 import React from 'react'
+import { useState } from 'react'
+import Search from '../Search/Search'
 import "./Banner.css"
 
 const Banner = () => {
+    const [showSearch, setShowSearch] = useState(false);
+
+
+
     return (
         <div className="banner">
+            <div className="banner__search">
+                {showSearch && <Search />}
+                <Button onClick={() => setShowSearch(!showSearch)} className="banner__searchButton" variant="outlined">Search Dates</Button>
+            </div>
             <div className="banner__info">
                 <h1>Get out and stretch your imagination.</h1>
                 <h5>Plan a different  kind of getaway to uncover the hidden gems near you.</h5>
