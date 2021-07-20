36453bdca4b6c6bac345804aca72376ef2fb668e
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-07-20 16:21:06 +0530

    Building Home component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="app">
-
+      <Home />
+      {/* header */}
+      {/* banner */}
+      {/* cards */}
+      {/* footer */}
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCAnBYmLVBM4dGq1yXM8149kEj6EG_cWMo",
+    authDomain: "airbnb-clone-d6989.firebaseapp.com",
+    projectId: "airbnb-clone-d6989",
+    storageBucket: "airbnb-clone-d6989.appspot.com",
+    messagingSenderId: "431976113643",
+    appId: "1:431976113643:web:f079a08c87606e353665ae"
+};
