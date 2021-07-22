2c06dfba661bad01f8cac33d3b4d7b513846894b
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-07-22 07:38:00 +0530

    Building Card component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import Banner from '../Banner/Banner'
+import Card from '../Card/Card'
 import "./Home.css"
 
 const Home = () => {
     return (
         <div className="home">
             <Banner />
+
+            <div className="home__section">
+                <Card />
+            </div>
         </div>
     )
 }
