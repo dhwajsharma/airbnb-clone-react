7f01896712955f7d2ffbab7f197bc814dee68b45
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-07-22 11:30:09 +0530

    Built Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,8 +3,9 @@ import "./Footer.css"
 
 const Footer = () => {
     return (
-        <div className="footer">
-
+        <div className='footer'>
+            <p>© 2021 Airbnb clone! No rights reserved - this is a demo!</p>
+            <p>Privacy · Terms · Sitemap · Company Details</p>
         </div>
     )
 }
