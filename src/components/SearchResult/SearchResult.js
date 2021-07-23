59e5e316c475f92378153fb388bcc73a99757ed1
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-07-23 09:42:18 +0530

    Building SearchResult.js

---
+++ src/components/SearchResult/SearchResult.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./SearchResult.css"
+
+const SearchResult = () => {
+    return (
+        <div className='searchResult'>
+
+        </div>
+    )
+}
+
+export default SearchResult
